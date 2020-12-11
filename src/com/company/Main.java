2cb56d15dfec3c8258fe9a7e79abb6121823bea6
Angelldlc <aaaldlc@gmail.com>
2020-12-11 13:48:29 +0100

    Cambio BUENAS MUNDO por ANGEL, añadido comentario encima de main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,8 @@ package com.company;
 
 public class Main {
 
-    //UwU
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("ANGEL");
+	// write your code her
+        System.out.println("BUENAS MUNDO");
     }
 }
